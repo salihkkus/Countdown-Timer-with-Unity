b68a273bfcfb60d0969e707f5345c03380cc82d6
salihkarakus <sskarakussalih77@gmail.com>
2025-02-13 17:01:23 +0300

    button and ınput filed added

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -1,36 +1,43 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Countdown : MonoBehaviour
 {
-
     [SerializeField] private Image timerImg;
     [SerializeField] private Text timerText;
-    [SerializeField] private float duration;
-    [SerializeField] private float currenttime;
-
+    [SerializeField] private InputField inputField; // Kullanıcının süre gireceği alan
+    [SerializeField] private Button startButton; // Zamanlayıcıyı başlatan buton
+    private float duration;
+    private float currenttime;
     
     void Start()
     {
-        currenttime = duration;
-        timerText.text = currenttime.ToString();
-        StartCoroutine(UpdateTime());
+        startButton.onClick.AddListener(StartCountdown);
+    }
+
+    private void StartCountdown()
+    {
+        if (float.TryParse(inputField.text, out duration) && duration > 0) // Kullanıcının girdisini al
+        {
+            currenttime = duration;
+            timerText.text = currenttime.ToString();
+            StartCoroutine(UpdateTime());
+        }
+        else
+        {
+            Debug.Log("Geçerli bir süre giriniz!");
+        }
     }
 
     private IEnumerator UpdateTime()
     {
-       while(currenttime >= 0)
-       {
-        timerImg.fillAmount = Mathf.InverseLerp(0 , duration , currenttime);
-        timerText.text = currenttime.ToString();
-        yield  return new WaitForSeconds(1f);
-        currenttime--;
-       }
-
-       yield return null;
+        while (currenttime >= 0)
+        {
+            timerImg.fillAmount = Mathf.InverseLerp(0, duration, currenttime);
+            timerText.text = currenttime.ToString();
+            yield return new WaitForSeconds(1f);
+            currenttime--;
+        }
     }
-
-    
 }
